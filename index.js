78c2099b95c38b659df446bf4dae284940b89cfe
UnaLai <qqq323618@gmail.com>
2020-01-01 16:42:58 +0800

    [feat]:update before ch6 content

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-//安裝
-const express = require('express')
-//啟用
-const app = express()
+const path = require('path');
 
-app.use('/',(req,res,next)=>{
-  console.log("It is homepage")
-  res.send('<h1>Hello from homepage!</h1>')
-})
+const express = require('express');
+const bodyParser = require('body-parser');
 
-app.listen(3000)
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
