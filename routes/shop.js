9e44da55c7aff2feaac0feedad94e9b58b69c1e5
UnaLai <qqq323618@gmail.com>
2020-01-01 18:23:05 +0800

    [feat]:shop.pug add content and use loop and if

--- routes/shop.js
+++ routes/shop.js
@@ -8,10 +8,9 @@ const router = express.Router();
 
 const adminData = require('./admin');
 
-
 router.get('/', (req, res, next) => {
-  console.log(adminData.products)
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products
+  res.render('shop',{prods:products, pageTitle: 'Shop'})
 });
 
 module.exports = router;
