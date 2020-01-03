7ff729f120cdf908fea2f828d915323fe56a135c
UnaLai <qqq323618@gmail.com>
2020-01-03 15:59:35 +0800

    [feat]:add ejs files of 404,add-product,shop

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('handlebars',expressHbs({layoutsDir:'views/layouts',defaultLayout:'main-layout',extname:'handlebars'}))
-app.set('view engine','handlebars')
+app.set('view engine','ejs')
 app.set('views','views')
 
 const adminData = require('./routes/admin');
