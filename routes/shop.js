44272fb8771b8a544f4bf4be66d83f272ed47036
UnaLai <qqq323618@gmail.com>
2020-01-03 14:20:51 +0800

    [feat]:render add path params

--- routes/shop.js
+++ routes/shop.js
@@ -10,7 +10,7 @@ const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
   const products = adminData.products
-  res.render('shop',{prods:products, pageTitle: 'Shop'})
+  res.render('shop',{prods:products, pageTitle: 'Shop',path:'/'})
 });
 
 module.exports = router;
